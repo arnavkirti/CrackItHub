4e9ab113be8ad4ff8c56a42a626693d453991855
Arnav Kirti <arnav.tkd@gmail.com>
2025-01-17 23:30:35 +0530

    feat: initialised frontend and added user schema

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )  
+}
+
+export default App
